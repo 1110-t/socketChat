35fef6c76dd5560fbf71651854b622c44a5c8889
katachi2honmachi <contact@miraino-katachih2.co.jp>
2022-12-15 11:43:07 +0900

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var listener = app.listen(3000, function() {
     console.log(listener.address().port);
 });
-app.get('/', function (req, res) {
-    res.send('ページが表示されました！');
+app.get('/', function(req, res) {
+    fs.readFile("./index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+    });
 });
